9f3d5d1529e7b008540a3f8f9db9d59972560b48
Jiew Wan Tan <jiewwan@gmail.com>
2018-11-01 08:48:12 +0800

    Update ddpg_agent.py

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -9,14 +9,13 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-
+BUFFER_SIZE = int(1e6)  # replay buffer size
+BATCH_SIZE = 1024        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR_ACTOR = 1e-3         # learning rate of the actor 
 LR_CRITIC = 1e-3        # learning rate of the critic
-WEIGHT_DECAY = 0.0000     # L2 weight decay
-BATCH_SIZE = 1024         # minibatch size
-BUFFER_SIZE = int(1e6)  # replay buffer size
+WEIGHT_DECAY = 0.0001   # L2 weight decay
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
